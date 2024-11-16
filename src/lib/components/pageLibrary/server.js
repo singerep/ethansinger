6be15d6f26263171d78abe3d4b1836c325f76889
Ethan Singer <ethansinger101@gmail.com>
2024-11-16 01:42:56 -0500

    changed back to sync

--- src/lib/components/pageLibrary/server.js
+++ src/lib/components/pageLibrary/server.js
@@ -39,7 +39,7 @@ async function loadAi2HtmlFiles(slug) {
 
 async function loadPageData(slug) {
     // fs.readFileSync(`/pages/${slug}/page.json`)
-    fs.readFile('/pages/2024-09-04-home/page.json')
+    fs.readFileSync('/pages/2024-09-04-home/page.json')
     // const page = JSON.parse(fs.readFileSync(`static/pages/${slug}/page.json`))
     // if (page.type == 'gdoc') {
     //     if (false) {
