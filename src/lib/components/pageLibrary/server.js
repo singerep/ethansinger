1e5becc24701f661c0786e7ea59fb7691221ad5b
Ethan Singer <ethansinger101@gmail.com>
2024-11-16 17:52:40 -0500

    trying fetch

--- src/lib/components/pageLibrary/server.js
+++ src/lib/components/pageLibrary/server.js
@@ -42,7 +42,8 @@ async function loadPageData(slug) {
     // fs.readFileSync(`/pages/${slug}/page.json`)
     console.log('Current directory: ' + process.cwd());
     console.log('Current directory: ' + path.join(process.cwd(), '/pages/2024-09-04-home/page.json'));
-    fs.readFileSync(path.join(process.cwd(), '/pages/2024-09-04-home/page.json'))
+    // fs.readFileSync(path.join(process.cwd(), '/pages/2024-09-04-home/page.json'))
+    console.log(await fetch('/pages/2024-09-04-home/page.json'))
     // const page = JSON.parse(fs.readFileSync(`static/pages/${slug}/page.json`))
     // if (page.type == 'gdoc') {
     //     if (false) {
