ddd0604222b63ce378bbfd36effc4eaa2d0caa8b
Ethan Singer <ethansinger101@gmail.com>
2024-11-16 18:17:13 -0500

    trying altered path

--- src/lib/components/pageLibrary/server.js
+++ src/lib/components/pageLibrary/server.js
@@ -42,7 +42,7 @@ async function loadPageData(slug) {
     // fs.readFileSync(`/pages/${slug}/page.json`)
     console.log('Current directory: ' + process.cwd());
     console.log('Current directory: ' + path.join(process.cwd(), '/pages/2024-09-04-home/page.json'));
-    console.log(fs.readFileSync(`src/lib/index.js`))
+    console.log(fs.readFileSync(`../../lib/index.js`))
     // fs.readFileSync(path.join(process.cwd(), '/pages/2024-09-04-home/page.json'))
     // const page = JSON.parse(fs.readFileSync(`static/pages/${slug}/page.json`))
     // if (page.type == 'gdoc') {
