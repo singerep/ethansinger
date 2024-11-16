48c3073b9860a43e828e4f8f2ea1547967aad9aa
Ethan Singer <ethansinger101@gmail.com>
2024-11-16 02:01:26 -0500

    trying joined path

--- src/lib/components/pageLibrary/server.js
+++ src/lib/components/pageLibrary/server.js
@@ -42,7 +42,7 @@ async function loadPageData(slug) {
     // fs.readFileSync(`/pages/${slug}/page.json`)
     console.log('Current directory: ' + process.cwd());
     console.log('Current directory: ' + path.join(process.cwd(), '/pages/2024-09-04-home/page.json'));
-    fs.readFileSync('/pages/2024-09-04-home/page.json')
+    fs.readFileSync(path.join(process.cwd(), '/pages/2024-09-04-home/page.json'))
     // const page = JSON.parse(fs.readFileSync(`static/pages/${slug}/page.json`))
     // if (page.type == 'gdoc') {
     //     if (false) {
